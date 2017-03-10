797d048b24c6a4c0d5d16a60275639c04d4f79b8
Kimberly Lalmansingh <kimberlylalmansingh@Kimberlys-MacBook-Pro.local>
2017-03-10 14:57:47 -0500

    Added comments

--- Mutation_Analysis.java
+++ Mutation_Analysis.java
@@ -4,11 +4,11 @@ import java.util.Random;
 public class Mutation_Analysis {
 
 	public static void main(String[] args){
-		int programRuns = 100;
-		int[] numOfMutations = new int[programRuns];
+		int programRuns = 100; //how many times to run analysis
+		int[] numOfMutations = new int[programRuns]; //array that will hold # of mutations in the last gen
 		
-		for(int i=0; i<programRuns; i++){
-			numOfMutations[i] = createArrays();
+		for(int i=0; i<programRuns; i++){ //for loop to run gen array creation 
+			numOfMutations[i] = createArrays(); //createArrays returns number of last gen mutations
 			System.out.println(numOfMutations[i]);
 		}
 		
@@ -16,13 +16,13 @@ public class Mutation_Analysis {
 	
 	public static void initialize(int[] arr){
 		Random rand = new Random();
-		for(int i=0; i<arr.length; i++){
-			int thing = 0;
-			if(arr[i]==1) continue;
-			else if(rand.nextDouble() <= .1){
-				thing = 1;
+		for(int i=0; i<arr.length; i++){ //for loop to go see if mutation will occur 
+			int thing = 0; // no mutation
+			if(arr[i]==1) continue; //if there was a previous mutation in slot, continue to next element
+			else if(rand.nextDouble() <= .01){ //see if element falls into probability of mutation
+				thing = 1; //mutation occured 
 			}
-			arr[i] = thing;
+			arr[i] = thing; //set element equal to mutation/ non-mutation
 		}
 //		for(int i=0; i<arr.length; i++){
 //			System.out.print(arr[i]);
@@ -30,11 +30,11 @@ public class Mutation_Analysis {
 //		System.out.println();
 	}
 	
-	public static int createArrays(){
-		int[] gen0 = new int[(int) Math.pow(2,0)];
-			initialize(gen0);
+	public static int createArrays(){ //create generation array function: this creates 10 generation plus the initial generation
+		int[] gen0 = new int[(int) Math.pow(2,0)]; //size of array is based on exponential growth by generation number
+			initialize(gen0); //function to mutate
 		int[] gen1 = new int[(int) Math.pow(2,1)];
-			checkMutations(gen0, gen1);
+			checkMutations(gen0, gen1); //checks for previous generation mutations
 			initialize(gen1);
 		int[] gen2 = new int[(int) Math.pow(2,2)];
 			checkMutations(gen1, gen2);
@@ -69,6 +69,9 @@ public class Mutation_Analysis {
 	}
 	
 	public static void checkMutations(int arr1[], int arr2[]){
+		//this function will check to see how many mutations occurred in the previous generation
+		//based on the number of mutations, double that amount of mutations will carry over into
+		//the current generation 
 		int childrenMutation = 0;
 		for(int i=0; i<arr1.length; i++){
 			if (arr1[i]==1){
@@ -85,6 +88,7 @@ public class Mutation_Analysis {
 	}
 	
 	public static int totalMutations(int[] gen10){
+		//function totals the number of mutations that occurred in the final generation 
 		int total = 0;
 		for (int i=0; i<gen10.length; i++){
 			if(gen10[i]==1){
